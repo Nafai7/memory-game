6fa5a95058bcee66e1347baf062d12ee0d279317
Nafai7 <nafailuet@gmail.com>
2023-04-03 21:13:51 +0200

    Added image matching. Added flip animations. resolve #4 resolve #6

--- script.js
+++ script.js
@@ -4,18 +4,60 @@ class Tile {
     }
 
     element() {
-        let img = document.createElement("img");
-        img.src = "images/" + this.imageNumber + ".png";
+        let questionMark = document.createElement("img");
+        questionMark.src = "images/question_mark.png";
+
+        let tileFront = document.createElement("div");
+        tileFront.className = "tile-front";
+        tileFront.appendChild(questionMark);
+
+        let catImg = document.createElement("img");
+        catImg.src = "images/" + this.imageNumber + ".png";
+        catImg.id = this.imageNumber;
+
+        let tileBack = document.createElement("div");
+        tileBack.className = "tile-back";
+        tileBack.appendChild(catImg);
 
         let tile = document.createElement("div");
         tile.className = "tile";
-        tile.appendChild(img);
+        tile.appendChild(tileFront);
+        tile.appendChild(tileBack);
+
+        tile.addEventListener("click", function() {tileClicked(this);})
 
         let container = document.createElement("div");
         container.className = "tile-container";
         container.appendChild(tile);
 
         return container;
+    }
+}
+
+function tileClicked(tile) {
+    let allTilesClicked = document.getElementsByClassName("tile-transform");
+    if (allTilesClicked.length < 2) {
+        tile.classList.toggle("tile-transform");
+
+        allTilesClicked = Array.from(document.getElementsByClassName("tile-transform"));
+
+        if (allTilesClicked.length >= 2) {
+            let board = document.getElementById("board");
+            board.classList.toggle("not-clickable");
+
+            setTimeout(function () {
+                if (allTilesClicked[0].lastChild.firstChild.id == allTilesClicked[1].lastChild.firstChild.id) {
+                    allTilesClicked.forEach(element => {
+                        element.remove();
+                    });
+                } else {
+                    allTilesClicked.forEach(element => {
+                        element.classList.toggle("tile-transform");
+                    });
+                }
+                board.classList.toggle("not-clickable");
+            }, 1000);
+        }
     }
 }
 
